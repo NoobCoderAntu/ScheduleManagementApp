95d58540fbf3a7619fd89666be88b36914b3aa8d
NoobCoderAntu <anupamdey2022cse@gmail.com>
2023-06-29 02:58:52 +0600

    initial commit

---
+++ src/main/java/com/anupam/springbootschedulemanagementapplication/SpringBootScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.anupam.springbootschedulemanagementapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScheduleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootScheduleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anupam/springbootschedulemanagementapplication/SpringBootScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupam.springbootschedulemanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootScheduleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
